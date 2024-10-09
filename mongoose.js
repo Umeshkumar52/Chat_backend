6b1d6ec4926a64e9ad08a4a536f1936b22d3e9f3
Umeshkumar52 <uk3993907@gmail.com>
2024-10-09 15:11:47 +0530

    add vercel.json file

--- mongoose.js
+++ mongoose.js
@@ -4,7 +4,7 @@ dotenv.config()
 const base_url="mongodb://localhost:27017/Chats_Database"
 const url=`mongodb+srv://Eangager:${process.env.MONGOOSE_PASSWORD}@cluster0.jke76.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 async function dbConnect(){
-    await mongoose.connect(url)
+    await mongoose.connect(base_url)
     console.log("database is connected");
 }
 export default dbConnect
