bc0ae18b77d8f03bb6f34cc5eab871161a76049d
Umeshkumar52 <123877527+Umeshkumar52@users.noreply.github.com>
2024-11-01 22:42:43 +0530

    Update publicIdHandler.js

--- midilwares/publicIdHandler.js
+++ midilwares/publicIdHandler.js
@@ -3,5 +3,5 @@ import crypto from 'crypto'
 export default function publicIdHandler(file,user_id,type){
   const fileContent=fs.readFileSync(file.path)
   const hash=crypto.createHash('md5').update(fileContent+Date.now()).digest('hex')
-  return `${type}/${user_id}/${hash}`
+  return `${type}_${user_id}_${hash}`
 }
