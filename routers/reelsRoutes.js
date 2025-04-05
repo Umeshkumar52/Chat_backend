902e06fb751f4fbf6d72ce3cb9593cab4032a6a1
Umeshkumar52 <uk3993907@gmail.com>
2025-04-05 14:29:13 +0530

    update

--- routers/reelsRoutes.js
+++ routers/reelsRoutes.js
@@ -4,13 +4,13 @@ import upload from '../midilwares/multer.js'
 import authentication from '../midilwares/authentication.js'
 const router=express.Router()
 router
-.post('/newReel/:user_id',upload.single('reel'),authentication,newReel)
+.post('/newReel/:user_id',upload.single('reel'),newReel)
 .get('/allReels/:offset/:limit',allReals)
 .get('/:_id',reel)
-.get('/:author',authentication,particularUserReel)
-.put('/commentToReel/:post_id',authentication,commentToReel)
-.get('/reelComment/:post_id',authentication,reel_Comments)
-.put('/likeToReel/:post_id/:author',authentication,likeToReel)
-.put('/disLikeToReel/:post_id/:author',authentication,disLikeToReel)
-.delete('/deleteReel/:reel_id/:public_id',authentication,deletReel)
+.get('/:author',particularUserReel)
+.put('/commentToReel/:post_id',commentToReel)
+.get('/reelComment/:post_id',reel_Comments)
+.put('/likeToReel/:post_id/:author',likeToReel)
+.put('/disLikeToReel/:post_id/:author',disLikeToReel)
+.delete('/deleteReel/:reel_id/:public_id',deletReel)
 export default router
